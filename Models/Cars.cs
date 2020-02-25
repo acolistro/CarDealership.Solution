5a82d5ac205bcb889c3eb71d76038f55fbada8fb
Hristo Dzhambov and Michelle Morin <hristo@>
2020-02-25 13:29:36 -0800

    add namespaces and separate logic into different files

---
+++ Models/Cars.cs
@@ -0,0 +1,23 @@
+namespace Dealership.Models
+{
+  public class Car
+  {
+    public string MakeModel;
+    public int Price;
+    public int Miles;
+    public string Message;
+
+    public Car(string makeModel, int price, int miles, string message)
+    {
+      MakeModel = makeModel;
+      Price = price;
+      Miles = miles;
+      Message = message;
+    }
+
+    public bool WorthBuying(int maxPrice, int maxMileage)
+    {
+      return (Price < maxPrice && Miles < maxMileage);
+    }
+  }
+}
--- Program.cs
+++ Program.cs
@@ -1,63 +1,46 @@
 ﻿using System;
 using System.Collections.Generic;
+using Dealership.Models;
 
-public class Car
+namespace Dealership
 {
-  public string MakeModel;
-  public int Price;
-  public int Miles;
-  public string Message;
-
-  public Car(string makeModel, int price, int miles, string message)
+  public class Program
   {
-    MakeModel = makeModel;
-    Price = price;
-    Miles = miles;
-    Message = message;
-  }
-
-  public bool WorthBuying(int maxPrice, int maxMileage)
-  {
-    return (Price < maxPrice && Miles < maxMileage);
-  }
-}
-
-public class Program
-{
-  public static void Main()
-  {
-    Car volkswagen = new Car("1974 Volkswagen Thing", 1100, 368792, "avoid this car");
-    Car yugo = new Car("1980 Yugo Koral", 700, 56000, "not your best choice");
-    Car ford = new Car("1988 Ford Country Squire", 1400, 239001, "an OK vehicle");
-    Car amc = new Car("1976 AMC Pacer", 400, 198000, "the budget choice");
-    
-    List<Car> Cars = new List<Car>() { volkswagen, yugo, ford, amc };
-
-    Console.WriteLine("Enter maximum price: ");
-    string stringMaxPrice = Console.ReadLine();
-    int maxPrice = int.Parse(stringMaxPrice);
-
-    Console.WriteLine("Enter a maximum mileage for a vehicle:");
-    string stringMaxMileage = Console.ReadLine();
-    int maxMileage = int.Parse(stringMaxMileage);
-
-    List<Car> CarsMatchingSearch = new List<Car>(0);
-
-    foreach (Car automobile in Cars)
+    public static void Main()
     {
-      if (automobile.WorthBuying(maxPrice, maxMileage))
+      Car volkswagen = new Car("1974 Volkswagen Thing", 1100, 368792, "avoid this car");
+      Car yugo = new Car("1980 Yugo Koral", 700, 56000, "not your best choice");
+      Car ford = new Car("1988 Ford Country Squire", 1400, 239001, "an OK vehicle");
+      Car amc = new Car("1976 AMC Pacer", 400, 198000, "the budget choice");
+      
+      List<Car> Cars = new List<Car>() { volkswagen, yugo, ford, amc };
+
+      Console.WriteLine("Enter maximum price: ");
+      string stringMaxPrice = Console.ReadLine();
+      int maxPrice = int.Parse(stringMaxPrice);
+
+      Console.WriteLine("Enter a maximum mileage for a vehicle:");
+      string stringMaxMileage = Console.ReadLine();
+      int maxMileage = int.Parse(stringMaxMileage);
+
+      List<Car> CarsMatchingSearch = new List<Car>(0);
+
+      foreach (Car automobile in Cars)
       {
-        CarsMatchingSearch.Add(automobile);
+        if (automobile.WorthBuying(maxPrice, maxMileage))
+        {
+          CarsMatchingSearch.Add(automobile);
+        }
       }
-    }
-    if (CarsMatchingSearch.Count == 0)
-    {
-      Console.WriteLine("There are no cars matching your search criteria.");
-    }
-    else {
-      foreach(Car automobile in CarsMatchingSearch)
+      if (CarsMatchingSearch.Count == 0)
       {
-          Console.WriteLine(automobile.MakeModel);
+        Console.WriteLine("There are no cars matching your search criteria.");
+      }
+      else {
+        foreach(Car automobile in CarsMatchingSearch)
+        {
+            Console.WriteLine(automobile.MakeModel);
+        }
       }
     }
   }
