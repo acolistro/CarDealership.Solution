e2babb9da372b7a289b8daa1df7770419d1e0aeb
Hristo Dzhambov and Michelle Morin <michelle@>
2020-02-25 08:24:31 -0800

    update method to consider users max mileage and only show cars that are within max price and max mileage

--- Program.cs
+++ Program.cs
@@ -6,17 +6,19 @@ public class Car
   public string MakeModel;
   public int Price;
   public int Miles;
+  public string Message;
 
-  public Car(string makeModel, int price, int miles)
+  public Car(string makeModel, int price, int miles, string message)
   {
     MakeModel = makeModel;
     Price = price;
     Miles = miles;
+    Message = message;
   }
 
-  public bool WorthBuying(int maxPrice)
+  public bool WorthBuying(int maxPrice, int maxMileage)
   {
-    return (Price < maxPrice);
+    return (Price < maxPrice && Miles < maxMileage);
   }
 }
 
@@ -24,10 +26,10 @@ public class Program
 {
   public static void Main()
   {
-    Car volkswagen = new Car("1974 Volkswagen Thing", 1100, 368792);
-    Car yugo = new Car("1980 Yugo Koral", 700, 56000);
-    Car ford = new Car("1988 Ford Country Squire", 1400, 239001);
-    Car amc = new Car("1976 AMC Pacer", 400, 198000);
+    Car volkswagen = new Car("1974 Volkswagen Thing", 1100, 368792, "avoid this car");
+    Car yugo = new Car("1980 Yugo Koral", 700, 56000, "not your best choice");
+    Car ford = new Car("1988 Ford Country Squire", 1400, 239001, "an OK vehicle");
+    Car amc = new Car("1976 AMC Pacer", 400, 198000, "the budget choice");
     
     List<Car> Cars = new List<Car>() { volkswagen, yugo, ford, amc };
 
@@ -35,11 +37,15 @@ public class Program
     string stringMaxPrice = Console.ReadLine();
     int maxPrice = int.Parse(stringMaxPrice);
 
+    Console.WriteLine("Enter a maximum mileage for a vehicle:");
+    string stringMaxMileage = Console.ReadLine();
+    int maxMileage = int.Parse(stringMaxMileage);
+
     List<Car> CarsMatchingSearch = new List<Car>(0);
 
     foreach (Car automobile in Cars)
     {
-      if (automobile.WorthBuying(maxPrice))
+      if (automobile.WorthBuying(maxPrice, maxMileage))
       {
         CarsMatchingSearch.Add(automobile);
       }
